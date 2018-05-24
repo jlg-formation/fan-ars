fc7794fdc68a1fdaa2509111b39a9485099325b1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-24 10:08:38 +0200

    ok

--- app/widget/widget.module.js
+++ app/widget/widget.module.js
@@ -3,11 +3,23 @@ const app = angular.module('widget', []);
 
 app.component('arsStar', {
     template: `
-    <img src="widget/img/yellow_star.png">
-    <img src="widget/img/yellow_star.png">
-    <img src="widget/img/yellow_star.png">
-    <img src="widget/img/white_star.png">
-    <img src="widget/img/white_star.png">
-    
-    `
+    <img ng-src="{{$ctrl.img[0]}}">
+    <img ng-src="{{$ctrl.img[1]}}">
+    <img ng-src="{{$ctrl.img[2]}}">
+    <img ng-src="{{$ctrl.img[3]}}">
+    <img ng-src="{{$ctrl.img[4]}}">
+    `,
+    controller: function ARSStarCtrl() {
+        const yellowStar = 'widget/img/yellow_star.png';
+        const whiteStar = 'widget/img/white_star.png';
+        this.img = [];
+
+        const note = 3;
+        for (let i = 0; i < note; i++) {
+            this.img[i] = yellowStar;
+        }
+        for (let i = note; i < 5; i++) {
+            this.img[i] = whiteStar;
+        }
+    }
 });
