d2acd41330fcc3dffba14f44819af36b9b0aba3d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2018-05-24 15:23:26 +0200

    ok

--- app/route/ProductCtrl.controller.js
+++ app/route/ProductCtrl.controller.js
@@ -1,5 +1,10 @@
-export default function ProductCtrl() {
+export default function ProductCtrl($http) {
+    'ngInject';
     this.start = () => {
         console.log('startxxx');
+
+        $http.get('ws/s1').then(response => {
+            console.log('s1 response', response);
+        }).catch();
     };
 };
--- server.js
+++ server.js
@@ -8,6 +8,22 @@ const app = express();
 app.use(express.static('.'));
 app.use(serveIndex('.', { icons: true }));
 
+app.use('/app/ws', (req, res, next) => {
+	const cfg = {
+		'/s1': 2,
+		'/s2': 1,
+		'/s3': 4,
+		'/s4': 5,
+		'/s5': 1,
+		'/s6': 2,
+	};
+	const time = cfg[req.url];
+	setTimeout(() => {
+		res.json({ url: req.url });
+	}, time * 1000);
+
+});
+
 app.use('/app/', (req, res, next) => {
 	res.sendFile('./app/index.html', { root: __dirname });
 });
